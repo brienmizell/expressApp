d36c9cdfa144a3012cf615edf9cfbfc2d52aa050
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2018-11-08 23:54:16 -0500

    added route matchers and route parameters

--- app.js
+++ app.js
@@ -2,13 +2,35 @@ let express = require(`express`);
 let app = express();
 
 // "/"      => "Hi there!"
-
 app.get('/', function(req, res) {
 	res.send('Hi there!');
 });
 
 // "/bye"   => "Goodbye!"
+app.get('/bye', function(req, res) {
+	res.send('Goodbye!!!');
+});
+
 // "/dog"   => "MEOW!"
+app.get('/dog', function(req, res) {
+	console.log('someone made a request to /dog');
+	res.send('MEOW!');
+});
+
+app.get('/r/:subredditName', function(req, res) {
+	var subreddit = req.params.subredditName;
+	res.send('WECLOME TO The ' + subreddit.toUpperCase() + ' SUBREDDIT!');
+});
+
+app.get('/r/:subredditName/comments/:id/:title/', function(req, res) {
+	res.send('WELCOME TO THE COMMENTS PAGE!!!');
+});
+
+app.get('*', function(req, res) {
+	res.send('you are a star!!!');
+});
 
 // Tell Express to listen for requests (start server)
-app.listen(3000, process.env.IP);
+app.listen(3000, process.env.IP, function() {
+	console.log('Server has started!!!');
+});
