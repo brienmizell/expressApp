c6127655688679fee52f778e20b8d1fe8062bb0b
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2018-11-09 21:58:50 -0500

    refactored animal sounds"

--- assignment1/assignment1.js
+++ assignment1/assignment1.js
@@ -5,17 +5,38 @@ app.get('/', function(req, res) {
 	res.send('Hi there, welcome to my assignment!!');
 });
 
-app.get('/speak/pig', function(req, res) {
-	res.send("The pig says 'Oink'");
+app.get('/speak/:animal', function(req, res) {
+	var animal = req.params.animal.toLowerCase();
+	var sounds = {
+		pig: 'Oink',
+		cow: 'Moo',
+		dog: 'Woof Woof',
+		cat: 'I hate you human',
+		goldfish: '...'
+	};
+	var sound = sounds[animal];
+	// if (animal === 'pig') {
+	// 	sound = 'oink';
+	// } else if (animal === 'cow') {
+	// 	sound = 'moo';
+	// } else if (animal === 'dog') {
+	// 	sound = 'woof woof';
+	// }
+
+	res.send('The ' + animal + ' says "' + sound + '"');
 });
 
-app.get('/speak/cow', function(req, res) {
-	res.send("The cow says 'Moo'");
-});
+// app.get('/speak/pig', function(req, res) {
+// 	res.send("The pig says 'Oink'");
+// });
 
-app.get('/speak/dog', function(req, res) {
-	res.send("The dog says'Woof Woof'");
-});
+// app.get('/speak/cow', function(req, res) {
+// 	res.send("The cow says 'Moo'");
+// });
+
+// app.get('/speak/dog', function(req, res) {
+// 	res.send("The dog says'Woof Woof'");
+// });
 
 app.get('*', function(req, res) {
 	res.send('Sorry, page not found...What are you doing with your life??');
